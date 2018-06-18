8b6fb8fed0ae32e243f5cbf58843b2176f6fa19f
Ana María Cárdenas Gasca <am.cardenas927@gmail.com>
2018-06-17 20:08:16 -0500

    add second part solution

--- part2.py
+++ part2.py
@@ -9,3 +9,25 @@ import sqlite3
 #  python3 part2.py orders cust=<customer id>
 #  python3 part2.py orders emp=<employee last name>
 
+conn = sqlite3.connect('Northwind_small.sqlite')
+c = conn.cursor()
+
+command = sys.argv[1]
+
+if (command == 'customers'):
+    print('ID Customer Name')
+    for row in c.execute("SELECT id, ContactName FROM 'Customer'"):
+        print(str(row[0]) + ' ' + row[1])
+elif (command == 'employees'):
+    print('ID   Employee Name')
+    for row in c.execute("SELECT id, FirstName, LastName FROM 'Employee'"):
+        print(str(row[0]) + ' ' + row[1] + ' ' + row[2])
+elif (command == 'orders'):
+    ord_params = sys.argv[2].split("=")
+    if (ord_params[0] == 'cust'):
+        for row in c.execute("SELECT Id, OrderDate FROM 'Order' WHERE CustomerId=?", (ord_params[1],)):
+            print(str(row[0]) + ' ' + row[1])
+    elif(ord_params[0] == 'emp'):
+        for row in c.execute("SELECT Id, OrderDate FROM 'Order' WHERE EmployeeId=?", (ord_params[1],)):
+            print(str(row[0]) + ' ' + row[1])
+    
